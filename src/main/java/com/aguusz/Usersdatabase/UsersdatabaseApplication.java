3ceb4a68517e0899b53974695fef285a017a687c
AguuSz <agus.sepu2000@hotmail.com>
2021-02-20 17:45:45 -0300

    Created custom HTTP responses

---
+++ src/main/java/com/aguusz/Usersdatabase/UsersdatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.aguusz.Usersdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersdatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersdatabaseApplication.class, args);
+	}
+
+}
